536f9064aeadffecb2f30ef07a942453ea935878
Kevin Torres <101214932+superkjr@users.noreply.github.com>
2022-05-06 12:12:54 -0600

    metodo desplegar registro

--- EJEMPLO_SEM7/RecursosHumanoCRUD/src/recursoshumanocrud/ConexionCRUD.java
+++ EJEMPLO_SEM7/RecursosHumanoCRUD/src/recursoshumanocrud/ConexionCRUD.java
@@ -68,4 +68,60 @@ public class ConexionCRUD {
             System.out.println("HA OCURRIDO EL SIGUIENTE ERROR: " + ex.getMessage());
         }
     }
+    public void desplegarRegistros(String tablaBuscar, String campoBuscar, String condicionBuscar) throws SQLException {
+        ConexionCRUD conectar = new ConexionCRUD();
+        Connection cone = conectar.getConnection();
+        try {
+            Statement stmt;
+            String sqlQueryStmt;
+            if (condicionBuscar.equals("")) {
+                sqlQueryStmt = "SELECT " + campoBuscar + " FROM " + tablaBuscar + ";";
+
+            } else {
+                sqlQueryStmt = " SELECT " + campoBuscar + " FROM " + tablaBuscar + " WHERE " + condicionBuscar;
+
+            }
+
+            stmt = cone.createStatement();
+            stmt.executeQuery(sqlQueryStmt);
+
+            try ( ResultSet miResultSet = stmt.executeQuery(sqlQueryStmt)) {
+
+                if (miResultSet.next()) {
+                    ResultSetMetaData metaData = miResultSet.getMetaData();
+                    int numColumnas = metaData.getColumnCount();
+                    System.out.println("<<REGISTROS ALMACENADOS>>");
+                    System.out.println();
+
+                    for (int i = 1; i <= numColumnas; i++) {
+
+                        System.out.printf("%-20s\t", metaData.getColumnName(i));
+                    }
+
+                    System.out.println();
+                    do {
+                        for (int i = 1; i <= numColumnas; i++) {
+                            System.out.printf("%-20s\t", miResultSet.getObject(i));
+
+                        }
+                        System.out.println();
+
+                    } while (miResultSet.next());
+                    System.out.println();
+
+                } else {
+                    System.out.println("NO SE HAN ENCONTRADO REGISTROS");
+                }
+                miResultSet.close();
+            } finally {
+
+                stmt.close();
+                cone.close();
+            }
+        } catch (SQLException ex) {
+
+            System.out.println("HA OCURRIDO EL SIGUIENTE ERROR: " + ex.getMessage());
+  
+        }
+    }
 }
