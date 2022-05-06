9732592a4c078b57b621aac3a2c654fdf09776c2
carlos-arman <minerocarlos28@gmail.com>
2022-05-06 12:45:48 -0700

    EJEMPLO SEMANA 7 TERMINADO

--- EJEMPLO_SEM7/RecursosHumanoCRUD/src/recursoshumanocrud/Create.java
+++ EJEMPLO_SEM7/RecursosHumanoCRUD/src/recursoshumanocrud/Create.java
@@ -17,8 +17,8 @@ public class Create {
         person.setTelPersona(leer.nextLine());
 
         String tabla = "tb_contacto";
-        String camposTabla = "nom_contacto,email.contacto,tel_contacto";
-        String ValoresCampos = "" + person.getNomPersona() + "','" + person.getEmailPersona() + "','" + person.getTelPersona() + "'";
+        String camposTabla = "nom_contacto,email_contacto,tel_contacto";
+        String ValoresCampos = "'" + person.getNomPersona() + "','" + person.getEmailPersona() + "','" + person.getTelPersona() + "'";
 
         ConexionCRUD utilerias = new ConexionCRUD();
 
