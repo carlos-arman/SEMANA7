f95676f845777aa638255fc3670c079bd54a4259
melvinantonio <melvinantonioangellopez@gmail.com>
2022-05-06 11:49:34 -0600

    Aporte a la clase

--- EJEMPLO_SEM7/RecursosHumanoCRUD/src/recursoshumanocrud/ConexionCRUD.java
+++ EJEMPLO_SEM7/RecursosHumanoCRUD/src/recursoshumanocrud/ConexionCRUD.java
@@ -30,4 +30,42 @@ public class ConexionCRUD {
     public Connection getConnection(){
         return conexion;
     }
+    
+    public void guardarRegistros(String tabla, String camposTabla, String valoresCampos){
+        ConexionCRUD conectar = new ConexionCRUD();
+        Connection cone = conectar.getConnection();
+        try{
+            String sqlQueryStmt = "INSERT INTO " + tabla + "(" + camposTabla + ") VALUES (" + valoresCampos +");";
+            Statement stmt;
+            stmt = cone.createStatement();
+            stmt.executeUpdate(sqlQueryStmt);
+            stmt.close();
+            cone.close();
+            System.out.println("Registro guardado correctamente!");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void actualizarEliminarRegistro(String tabla, String valoresCamposNuevos, String condicion) {
+        ConexionCRUD conectar = new ConexionCRUD();
+        Connection cone = conectar.getConnection();
+        try {
+            Statement stmt;
+            String sqlQueryStmt;
+
+            if (valoresCamposNuevos.isEmpty()) {
+                sqlQueryStmt = "DELETE FROM " + tabla + " WHERE " + condicion + ";";
+            } else {
+                sqlQueryStmt = "UPDATE " + tabla + " SET " + valoresCamposNuevos + " WHERE " + condicion + ";";
+
+            }
+            stmt = cone.createStatement();
+            stmt.executeUpdate(sqlQueryStmt);
+            stmt.close();
+            cone.close();
+        } catch (SQLException ex) {
+            System.out.println("HA OCURRIDO EL SIGUIENTE ERROR: " + ex.getMessage());
+        }
+    }
 }
