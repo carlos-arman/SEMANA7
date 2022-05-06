c9d3d70c0663b23b8d9ecdcb8aec5cd79e42d8b2
Kevin Torres <101214932+superkjr@users.noreply.github.com>
2022-05-06 12:06:23 -0600

    menu principal

--- EJEMPLO_SEM7/RecursosHumanoCRUD/src/recursoshumanocrud/MenuPrincipal.java
+++ EJEMPLO_SEM7/RecursosHumanoCRUD/src/recursoshumanocrud/MenuPrincipal.java
@@ -3,15 +3,55 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package recursoshumanocrud;
-
-
+import java.sql.*;
+import java.util.Scanner;
+/**
+ *
+ * @author Armando
+ */
 public class MenuPrincipal {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws SQLException {
+        desplegarMenu();
     }
-    
+    public static void desplegarMenu() throws SQLException {
+        Scanner leer = new Scanner(System.in);
+
+        String opcionMenu;
+        //Menu a desplegar
+        System.out.println("**************************************");
+        System.out.println("|      CRUD DE JAVA EN CONSOLA       |");
+        System.out.println("|  Opciones                          |");
+        System.out.println("|        1. Crear registro           |");
+        System.out.println("|        2. Consultar Registro       |");
+        System.out.println("|        3. Actualizar registro      |");
+        System.out.println("|        4. Eliminar Registro        |");
+        System.out.println("|        5. Salir                    |");
+        System.out.println("**************************************");
+        System.out.println("Seleccionar Opcion");
+
+        opcionMenu = leer.next();
+
+        switch(opcionMenu){
+            case "1" :
+            //Create create = new Create();
+            break;
+            case "2":
+                //Read read = new Read();
+            break;
+            case "3" :
+                //Update update = new Update();
+            break;
+            case "4" :
+                //Delete delete = new Delete();
+            break;
+            case "5":
+                
+            System.exit(0);
+            break;
+            default:
+            System.out.println("Seleccion invalida!");
+            break;
+        }
+
+    }
 }
